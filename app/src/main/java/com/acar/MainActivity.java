40f380172957a7df57c9cd37a1a715406d3b33a3
Silbrother <silton.2009@hotmail.com>
2019-02-17 13:39:14 -0300

    Grafico funcionando na tela fragment

--- app/src/main/java/com/acar/MainActivity.java
+++ app/src/main/java/com/acar/MainActivity.java
@@ -1,26 +1,23 @@
 package com.acar;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionBarContextView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.widget.Switch;
-import android.widget.TextView;
-
-import static com.acar.R.layout.fragment_fragment_graficco;
-
+import com.acar.grafico;
 public class MainActivity extends AppCompatActivity {
 
     /**
@@ -127,6 +124,9 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case 3:
                     rootView = inflater.inflate(R.layout.content_grafico, container, false);
+                   /*chama a classe grafico*/
+                    Intent intent = new Intent(getActivity(), grafico.class);
+                    startActivity(intent);
                     break;
                 default:
             }
--- app/src/main/java/com/acar/grafico.java
+++ app/src/main/java/com/acar/grafico.java
@@ -7,7 +7,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class grafico extends AppCompatActivity {
+    float itensGrafico[] = {48.6f, 56.8f, 45.7f, 10.5f, 35.2f, 25.6f,36.78f,8.45f,15.6f,32.4f};
+    String descricao[] = {"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+    PieChart grafico;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +27,20 @@ public class grafico extends AppCompatActivity {
         setContentView(R.layout.activity_grafico);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        /*Grafico implementado!*/
+        grafico = (PieChart) findViewById(R.id.graficoID);
+        List<PieEntry> entradasGrafico = new ArrayList<>();
+        for (int i = 0; i < itensGrafico.length; i++){
+            entradasGrafico.add(new PieEntry(itensGrafico[i],descricao[i]));
+    }
 
+        PieDataSet dataSet = new PieDataSet(entradasGrafico,  "Legenda do Gráfico");
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        PieData pieData = new PieData(dataSet);
+        grafico.animateY(1300);
+        grafico.setData(pieData);
+        grafico.invalidate();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
